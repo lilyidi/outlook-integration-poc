cabacfcdb60c2f8f3684ebd1cc9d048aa3b3d07c
Vinodh Periyasamy <vinodhkps@users.noreply.github.com>
2024-10-07 17:05:40 -0400

    ability to group messages by thread

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const { Client } = require('@microsoft/microsoft-graph-client');
 const fs = require('fs');
 const path = require('path');
-require('isomorphic-fetch'); 
+require('isomorphic-fetch');
 require('dotenv').config();
 const { ConfidentialClientApplication } = require('@azure/msal-node');
 
@@ -34,39 +34,39 @@ const config = {
 
 function getAuthenticatedClient(accessToken) {
   return Client.init({
-      authProvider: (done) => {
-          done(null, accessToken); // Pass the access token
-      },
+    authProvider: (done) => {
+      done(null, accessToken); // Pass the access token
+    },
   });
 }
-  
+
 
 const redirectHost = process.env.REDIRECT_HOST
 
 const msalClient = new ConfidentialClientApplication(config);
 
 const createSubscription = async (accessToken, homeAccountId, username) => {
-    // const accessToken = 'YOUR_ACCESS_TOKEN';  // Get the access token using OAuth flow
-    const subscriptionData = {
-      "changeType": "created,updated",
-      "notificationUrl": `${redirectHost}/webhook`,
-      "resource": "me/messages",
-      "expirationDateTime": new Date(Date.now() + 30000).toISOString(),  // 5 minutes from now
-      "clientState": `${username}`
-    };
-  
-    try {
-      const response = await axios.post('https://graph.microsoft.com/v1.0/subscriptions', subscriptionData, {
-        headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      console.log('Subscription created:', response.data);
-    } catch (error) {
-      console.error('Error creating subscription:', error.response.data);
-    }
+  // const accessToken = 'YOUR_ACCESS_TOKEN';  // Get the access token using OAuth flow
+  const subscriptionData = {
+    "changeType": "created,updated",
+    "notificationUrl": `${redirectHost}/webhook`,
+    "resource": "me/messages",
+    "expirationDateTime": new Date(Date.now() + 30000).toISOString(),  // 5 minutes from now
+    "clientState": `${username}`
+  };
+
+  try {
+    const response = await axios.post('https://graph.microsoft.com/v1.0/subscriptions', subscriptionData, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    console.log('Subscription created:', response.data);
+  } catch (error) {
+    console.error('Error creating subscription:', error.response.data);
+  }
 };
 
 // Session setup
@@ -95,10 +95,10 @@ app.get('/auth/callback', (req, res) => {
   const tokenRequest = {
     code: req.query.code,
     scopes: ["user.read", "mail.readwrite"],
-    redirectUri:`${redirectHost}/auth/callback`,
+    redirectUri: `${redirectHost}/auth/callback`,
   };
 
-  
+
 
   msalClient.acquireTokenByCode(tokenRequest).then((response) => {
     if (response.account) {
@@ -116,15 +116,15 @@ app.get('/auth/callback', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-    if (req.query.validationToken) {
-      res.send(req.query.validationToken);  // Respond with the validation token
-    } else {
-      const notification = req.body
-      if (notification) {
-        console.log(`New email received or updated: ${JSON.stringify(notification)}` );
-      }
-      res.status(202).send();  // Acknowledge the notification
+  if (req.query.validationToken) {
+    res.send(req.query.validationToken);  // Respond with the validation token
+  } else {
+    const notification = req.body
+    if (notification) {
+      console.log(`New email received or updated: ${JSON.stringify(notification)}`);
     }
+    res.status(202).send();  // Acknowledge the notification
+  }
 });
 
 // Get emails
@@ -132,20 +132,74 @@ app.get('/emails', async (req, res) => {
   if (!req.session.accessToken) {
     return res.redirect('/login');
   }
+  const showGrouped = req.query.showGrouped || 'false';
   const client = getAuthenticatedClient(req.session.accessToken);
   try {
     let messages = [];
     const queryOptions = {
-      '$select': 'id,subject,body,bodyPreview,conversationId,conversationIndex,internetMessageHeaders',
+      '$select': 'id,subject,body,bodyPreview,conversationId,conversationIndex,internetMessageHeaders,receivedDateTime,from,toRecipients',
       '$expand': 'attachments',
       '$top': 25 // Limit the number of messages to fetch
     };
-    let nextPageUrl = `/me/messages?${new URLSearchParams(queryOptions).toString()}`;
+    let nextPageUrl = `/me/mailFolders/inbox/messages?${new URLSearchParams(queryOptions).toString()}`;
     while (nextPageUrl) {
       const response = await client.api(nextPageUrl).get();
       messages = messages.concat(response.value);
-      // nextPageUrl = response['@odata.nextLink'];
-      nextPageUrl = false;
+      nextPageUrl = response['@odata.nextLink'];
+    }
+
+    if (showGrouped == 'true') {
+      messages = messages.sort((a, b) => new Date(a.receivedDateTime) - new Date(b.receivedDateTime));
+      let preProcessDict = {}
+      for (let msg of messages) {
+        if (msg.internetMessageHeaders && msg.internetMessageHeaders.length > 0) {
+          const messageIdHeader = msg.internetMessageHeaders.find(header => header.name === "Message-ID");
+          if (messageIdHeader) {
+            preProcessDict[messageIdHeader.value] = {}
+            preProcessDict[messageIdHeader.value].msg = msg;
+            const referencesHeader = msg.internetMessageHeaders.find(header => header.name === "References");
+            if (referencesHeader) {
+              preProcessDict[messageIdHeader.value].references = referencesHeader.value;
+            }
+          }
+        }
+      }
+
+      let threadedDict = {}
+      Object.entries(preProcessDict).forEach(([key, value]) => {
+        if (!value.references) {
+          value.threadId = key;
+        }
+        else {
+          const references = value.references.split(' ');
+          let foundThreadId = null;
+          for (const ref of references) {
+            if (preProcessDict[ref] && preProcessDict[ref].threadId) {
+              foundThreadId = preProcessDict[ref].threadId;
+              break;
+            }
+          }
+          if (foundThreadId) {
+            value.threadId = foundThreadId;
+          } else {
+            value.threadId = key; // If no threadId found, use the current message ID as threadId
+          }
+        }
+      });
+
+      let groupedMessages = {};
+      Object.entries(preProcessDict).forEach(([key, value]) => {
+        const threadId = value.threadId;
+        if (!groupedMessages[threadId]) {
+          groupedMessages[threadId] = [];
+        }
+        groupedMessages[threadId].push(value.msg);
+      });
+      // console.log(`pre-process-dict`);
+      // const filePath1 = path.join(__dirname, `email-${req.session.username}.json`);
+      // fs.writeFileSync(filePath1, JSON.stringify(groupedMessages));
+      res.render('grouped-email', { groupedMessages: groupedMessages });
+      return;
     }
     res.render('emails', { emails: messages });
   } catch (error) {
@@ -225,17 +279,17 @@ app.post('/extend-subs', async (req, res) => {
     for (let message of messages) {
       const newExpirationDateTime = new Date(Date.now() + 3600 * 1000).toISOString();
       try {
-          const updatedSubscription = await client.api(`/subscriptions/${message.id}`)
-              .patch({
-                  expirationDateTime: newExpirationDateTime
-              });
+        const updatedSubscription = await client.api(`/subscriptions/${message.id}`)
+          .patch({
+            expirationDateTime: newExpirationDateTime
+          });
 
-          console.log('Subscription renewed successfully:', updatedSubscription);
+        console.log('Subscription renewed successfully:', updatedSubscription);
       } catch (error) {
-          console.error('Error renewing subscription:', error);
+        console.error('Error renewing subscription:', error);
       }
     }
-  
+
     res.render('emails', { emails: messages });
   } catch (error) {
     res.status(500).send(error);
